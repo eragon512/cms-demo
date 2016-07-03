5c1ad2e2c74db835454691ffbf2892a0a69c089b
Anirud Samala <anirud.samala@gmail.com>
2016-07-04 01:09:37 +0530

    Added auto-load functionality to branch dropdown

---
+++ model/load_student_branch_list.php
@@ -0,0 +1,14 @@
+<?php
+	function load_student_branch_list() {
+		require("load_database.php");
+		$connect = load_database();
+
+		$branch_result = mysqli_query($connect,"SELECT DISTINCT student_branch FROM students;");
+		$branch_list = [];
+		while($branch = mysqli_fetch_array($branch_result,MYSQLI_ASSOC)) {
+			array_push($brach_list,$branch["student_branch"]);
+		}
+
+		mysqli_close($connect);
+		return $branch_list;
+	}
