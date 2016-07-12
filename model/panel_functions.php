1c7fdbd0dcf381f4b2d87af8db0168af42fab3f5
Anirud Samala <anirud.samala@gmail.com>
2016-07-12 06:49:51 +0530

    Removed name from panel div

--- model/panel_functions.php
+++ model/panel_functions.php
@@ -88,7 +88,7 @@
 				}
 
 				foreach($panel_list as $panel) {
-					echo "<div name='panel{$panel["panel_id"]}' id=".(int)$panel["panel_child_id"]." class='panel {$panel["panel_class"]}' style='height:{$panel["panel_height"]}%; width:{$panel["panel_width"]}%;' >\n";
+					echo "<div id=".(int)$panel["panel_child_id"]." class='panel {$panel["panel_class"]}' style='height:{$panel["panel_height"]}%; width:{$panel["panel_width"]}%;' >\n";
 						array_push($GLOBALS['panel_tracker'],(int)$panel["panel_child_id"]);
 						$visited[(int)$panel["panel_child_id"]] = true;
 						
@@ -103,7 +103,7 @@
 							}
 							else if($mode === "page-edit") {
 								if(isset($panel["page_id"]) && $panel["page_id"] === $page_id) {
-									echo "<textarea name={$panel["panel_child_id"]}>{$panel["panel_data"]}{$panel["page_id"]}</textarea><br>";
+									echo "<textarea name={$panel["panel_child_id"]}>{$panel["panel_data"]}{$panel["page_id"]}</textarea><br><button name=></button>";
 								} else {
 									echo "<textarea name={$panel["panel_child_id"]}></textarea><br>";
 								}
