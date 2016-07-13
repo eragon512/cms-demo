bcecca467c3d723f232d3575540849e852f06090
Anirud Samala <anirud.samala@gmail.com>
2016-07-14 03:42:08 +0530

    Revert "Added DB view functionality"
    This reverts commit c2c4d5c3a4b9790de01438010d49b85c1d6d9e9f.
    
    Undid Trimming

--- views/view_client_database_list.php
+++ views/view_client_database_list.php
@@ -1,13 +1,7 @@
 <?php
 	require_once('../model/client_database_functions.php');
 	if($_SERVER["REQUEST_METHOD"] === "POST") {
-		if($_POST["submit"] === "create") {
-			create_client_database($_POST);
-		} else if($_POST["submit"] === "edit") {
-			edit_client_database($_POST);
-		} else if($_POST["submit"] === "delete") {
-			delete_client_database($_POST);
-		}
+		create_client_database();
 	}
 	$client_database_list = load_client_database_list();
 ?>
@@ -18,37 +12,56 @@
 	</head>
 	<body>
 		List of Databases: 
-		<table>
-			<tr>
-				<td>Database Name</td>
-				<td>Database Server</td>
-				<td>Database Username</td>
-				<td>Database Password</td>
-			</tr>
+		<ul>
 			<?php
 				foreach($client_database_list as $client_database) {
-					echo "<tr><form action='' method='POST'>";
-						echo "<input type='hidden' name='client_db_id' value={$client_database["client_db_id"]} />";
-						echo "<td><a target='_blank' href='view_client_database_table_list.php?db_id={$client_database["client_db_id"]}'>{$client_database["client_db_name"]}</a></td>";
-						echo "<td><input type='text' name='client_db_server' value='{$client_database["client_db_server"]}' /></td>";
-						echo "<td><input type='text' name='client_db_username' value='{$client_database["client_db_username"]}' /></td>";
-						echo "<td><input type='text' name='client_db_password' value='{$client_database["client_db_password"]}' /></td>";
-						echo "<td><button type='submit' name='submit' value='edit'>edit</button></td>";
-						echo "<td><button type='submit' name='submit' value='delete'>delete</button></td>";
-					echo "</form></tr>\n";
+					echo "<li><a href='view_client_database.php?db_server={$client_database["client_db_server"]}&db_user={$client_database["client_db_username"]}&db_pass={$client_database["client_db_password"]}&db_name={$client_database["client_db_name"]}'>{$client_database["client_db_name"]}</a> - server: {$client_database["client_db_server"]}, username: {$client_database["client_db_username"]} </li>\n";
 				}
 			?>
-		</table>
+		</ul>
 		
 		<br>
 		New Database:
 		<form name="client_database" action="" method="POST">
-			Database Name: <input type="text" name="client_database_name" /> <br>
-			Server Name: <input type="text" name="client_database_server" /> <br>
-			Database Access Username: <input type="text" name="client_database_username" /> <br>
-			Database Access Password: <input type="text" name="client_database_password" /> <br>
-			<button type="submit" name="submit" value="create">Create Database</button>
+			<button type="button" onclick="javascript: add_client_database();">Add new database</button>
+			<button type="submit" value="Submit">Create Databases</button>
 		</form>
+		
+		<script>
+			var counter = 1;
+			
+			var add_client_database = function() {
+				//console.log("Hello");
+				var client_database_server = document.createElement('input');
+				client_database_server.type = "text";
+				client_database_server.name = "client_database_"+counter+"_server";
+				client_database_server.placeholder = "Server Name";
+				
+				var client_database_username = document.createElement('input');
+				client_database_username.type = "text";
+				client_database_username.name = "client_database_"+counter+"_username";
+				client_database_username.placeholder = "Database access Username";
 
+				var client_database_password = document.createElement('input');
+				client_database_password.type = "text";
+				client_database_password.name = "client_database_"+counter+"_password";
+				client_database_password.placeholder = "Database access Password";
+
+				var client_database_name = document.createElement('input');
+				client_database_name.type = "text";
+				client_database_name.name = "client_database_"+counter+"_name";
+				client_database_name.placeholder = "Database Name";
+
+				var client_database = document.createElement('div');
+				client_database.appendChild(client_database_server);
+				client_database.appendChild(client_database_username);
+				client_database.appendChild(client_database_password);
+				client_database.appendChild(client_database_name);
+				client_database.innerHTML += '<br />';
+				
+				document.getElementsByName("client_databases")[0].appendChild(client_database);
+				counter += 1;
+			}
+		</script>
 	</body>
 </html>
--- views/view_client_database_table.php
+++ views/view_client_database_table.php
@@ -1,10 +1,17 @@
 <?php
-	require('../model/client_database_functions.php');
-
-	$db_id = $_GET["db_id"];
+	require('../model/load_client_database_table.php');
+	$server = $_GET["db_server"];
+	$username = $_GET["db_user"];
+	$password = $_GET["db_pass"];
+	$db_name = $_GET["db_name"];
 	$table_name = $_GET["table_name"];
-	$database = load_client_database($db_id);
-	$table = load_query_table(load_client_database_table($db_id,$table_name));
+	$sql_query = isset($_GET["sql_query"]) ? $_GET["sql_query"] : "";
+
+	if(!isset($table_name)) {
+		die("Unable to load Database");
+	} else if(!($client_database_table = load_client_database_table($server,$username,$password,$db_name,$table_name,$sql_query))) {
+		die("Unable to load Database");
+	};
 ?>
 
 <!DOCTYPE html>
@@ -12,9 +19,40 @@
 	<head>
 	</head>
 	<body>
-		Table <?php echo $table_name; ?> :
-		<?php
-			echo $table;
-		?>
+		Table <?php echo $_GET["table_name"]; ?> :
+		<table>
+			<tr>
+				<?php
+					foreach($client_database_table["schema"] as $table_field) {
+						echo "<th>{$table_field}</th>";
+					}
+					unset($table_field);
+				?>
+			</tr>
+			<?php
+				foreach($client_database_table["data"] as $table_row) {
+					echo "<tr>\n";
+						foreach($table_row as $value) {
+							echo "<td> {$value} </td>\n";
+						}
+						unset($value);
+					echo "</tr>\n";
+				}
+				unset($table_row);
+			?>
+		</table>
+
+		<br>
+		<form name="sql_query" action=<?php echo $_SERVER['PHP_SELF']; ?> method="GET" >
+			Run SQL Query: <input type="text" name="sql_query" />
+			<input type="hidden" name="db_server" value=<?php echo $server; ?> />
+			<input type="hidden" name="db_user" value=<?php echo $username; ?> />
+			<input type="hidden" name="db_pass" value=<?php echo $password; ?> >
+			<input type="hidden" name="db_name" value=<?php echo $db_name; ?> />
+			<input type="hidden" name="table_name" value=<?php echo $table_name; ?> />
+			
+			<button type="submit">Submit SQL Query</button>
+		</form>
+
 	</body>
 </html>
